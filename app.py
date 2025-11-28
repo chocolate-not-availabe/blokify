9c5284d7109ad7f3d88a83f583d624e9245e804f
chocolate-not-availabe <zoyannbarrett05@gmail.com>
2025-11-28 14:51:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -393,4 +393,7 @@ def get_progress(story_id):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    import os
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
